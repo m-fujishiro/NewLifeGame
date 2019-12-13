88862624eb11fe8dcc6ca2b1b4f63477acc2817c
m-fujishiro <fujishiro.business@gmail.com>
2019-12-13 12:44:03 +0900

    白セルをクリックで選択、その後灰セルクリックで入れ替え

--- Script1.js
+++ Script1.js
@@ -15,4 +15,33 @@ for (let i = 0; i < 2; i++) {
     $(black).removeClass("none");
 }
 
+function White(this_selector) {
+    //選択状態を取得する
+    var isSelected = $(this_selector).hasClass("selected");
+    //全部のselected属性を消す
+    $(".selected").removeClass("selected");
+    // $(this)にisSelectedがfalseならaddClassする
+    if (!isSelected) $(this_selector).addClass("selected");
+}
+
+function None(this_selector) {
+    var selected = $(".selected");
+    //選択状態が無かったら終了
+    if ($(selected).length < 1) return;
+    console.assert($(selected).length === 1, "選択状態が複数存在します。");
+
+    console.log($(selected).attr("class"));
+
+    $(this_selector).removeClass();
+    $(this_selector).addClass($(selected).attr("class"));
+    $(this_selector).css("color", $(selected).attr("color"));
+    $(selected).removeClass();
+    $(selected).addClass("cell none");
+    $(this_selector).removeClass("selected");
+}
+
+$(".cell").click(function () {
+    if ($(this).hasClass("white")) White($(this));
+    if ($(this).hasClass("none")) None($(this));
+});
 
